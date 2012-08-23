2ba9e4648bb9cc1657746a8a9aecf77a7d6ca50b
MehdiK <me@mehdi-khalili.com>
2012-08-23 19:55:59 +1000

    Added cache manifest support - WIP

---
+++ PhoneBook/Controllers/OfflineSupportController.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System.Web.Mvc;
+
+namespace PhoneBook.Controllers
+{
+    [Authorize]
+    public class OfflineSupportController : Controller
+    {
+         public PartialViewResult AppCacheManifest()
+         {
+             return PartialView();
+         }
+
+        public ViewResult OnlineOnlyPage()
+        {
+            return View();
+        }
+    }
+}
