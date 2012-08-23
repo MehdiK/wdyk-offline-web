5d9e889f3b1b08e9e2630ec6ad9d830a38b73fdd
MehdiK <me@mehdi-khalili.com>
2012-08-23 20:09:05 +1000

    Now appcache manifest added to the offline pages only

---
+++ PhoneBook/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace PhoneBook.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString ConditionalManifest(this HtmlHelper helper)
+        {
+            var url = new UrlHelper(helper.ViewContext.RequestContext);
+            if (helper.ViewBag.SupportsOffline != null)
+                return new MvcHtmlString(string.Format("manifest ='{0}'", url.Action("AppCacheManifest", "OfflineSupport")));
+
+            return new MvcHtmlString(string.Empty);
+        }
+    }
+}
