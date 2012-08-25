4de90c041f38c66621a9062a2079d88911489666
MehdiK <me@mehdi-khalili.com>
2012-08-25 13:41:24 +1000

    Added contact edit pages to offline cache

--- PhoneBook/Controllers/OfflineSupportController.cs
+++ PhoneBook/Controllers/OfflineSupportController.cs
@@ -1,11 +1,14 @@
-﻿using System.Text;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using PhoneBook.Data;
+using System.Linq;
 
 namespace PhoneBook.Controllers
 {
     [Authorize]
     public class OfflineSupportController : Controller
     {
+        readonly DataContext _dataContext = new DataContext();
+
          public PartialViewResult AppCacheManifest()
          {
              return PartialView();
@@ -14,6 +17,12 @@ namespace PhoneBook.Controllers
         public ViewResult OnlineOnlyPage()
         {
             return View();
+        }
+
+        public ActionResult ContactsEdit()
+        {
+            var contacts = _dataContext.Contacts.Where(c => c.UserName == User.Identity.Name).Select(c => c.Id).ToList();
+            return PartialView(contacts);
         }
     }
 }
